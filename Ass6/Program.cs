29f07111aa72385c44feb115f432b81ec7f4a0cc
ahilan <ahilan_ak@hotmail.com>
2013-04-09 16:03:00 -0400

    Assigment6

---
+++ Ass6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Ass6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string end = "X";
+            const string DELIM = ",";
+            string name;
+            int age;
+            data ifile = new data();
+            FileStream outFile = new FileStream("c:/testfile.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outFile);
+
+            Console.WriteLine("Enter employee name or " + end + " to quit >>");
+            ifile.Name = Console.ReadLine();
+
+            while (ifile.Name != end)
+            {
+                Console.Write("Enter the name >> ");
+                name = Console.ReadLine();
+                Console.Write("Enter age >> ");
+                age = Convert.ToInt16(Console.ReadLine());
+                writer.WriteLine(name + DELIM + age);
+                //Console.WriteLine("Enter employee name or " + end + " to quit >>");
+                //ifile.Name = Console.ReadLine();
+
+
+            }
+
+
+            writer.Close();
+            outFile.Close();
+        }
+    }
+}
---
+++ Ass6/data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ass6
+{
+    class data
+    {
+        public string Name { get; set; }
+        public int age { get; set; }
+    }
+}
